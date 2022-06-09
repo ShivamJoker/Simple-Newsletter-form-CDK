5a3b19e056d78a826a443b6d12af5b729fb1ce88
Shivam <singhshivamkr@gmail.com>
2022-06-09 15:27:07 +0530

    :tada: Create stack of APIgw, Lambda, DynamoDB

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,16 +1,56 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from "aws-cdk-lib";
+import { Table, AttributeType, BillingMode } from "aws-cdk-lib/aws-dynamodb";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
+import { Construct } from "constructs";
+import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
 
 export class BackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const db = new Table(this, "user-db-table", {
+      tableName: "learnaws-newsletter",
+      partitionKey: { name: "email", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const subscribeFn = new Function(this, "subscribeHanlderFn", {
+      functionName: "handle-form-req",
+      code: Code.fromAsset(join(__dirname, "../lambda/dist")),
+      handler: "handleFormReq.main",
+      runtime: Runtime.NODEJS_16_X,
+      environment: {
+        TABLE_NAME: db.tableName,
+      },
+    });
+
+    // allow out lambda to write to the DB
+    db.grantWriteData(subscribeFn);
+
+    // create integration for APIGW v2
+    const subscribeFnIntegration = new HttpLambdaIntegration(
+      "handleFormReqIntegration",
+      subscribeFn
+    );
+
+    const httpApi = new HttpApi(this, "subscribe-endpoint");
+
+    if (!httpApi.url) {
+      throw Error("Failed to get http api url");
+    }
+
+    // Add routes in HTTP API
+    httpApi.addRoutes({
+      path: "/subscribe",
+      methods: [HttpMethod.POST],
+      integration: subscribeFnIntegration,
+    });
+
+    new CfnOutput(this, "endpoint", {
+      value: httpApi.url!,
+    });
   }
 }
